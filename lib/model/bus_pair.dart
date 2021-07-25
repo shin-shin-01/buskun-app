5c11f5c7e1ae497492e8344a75fff682ad13dc91
Kaze Shindo <kaze.bknb@outlook.jp>
2021-07-25 22:31:36 +0900

    refactoring: bus_pair list

---
+++ lib/model/bus_pair.dart
@@ -0,0 +1,9 @@
+class BusPair {
+  final String origin;
+  final String destination;
+
+  BusPair({
+    this.origin,
+    this.destination,
+  });
+}
--- lib/ui/favorite/favorite.dart
+++ lib/ui/favorite/favorite.dart
@@ -28,7 +28,6 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
               Expanded(
                 child: Container(
                   width: double.infinity,
-                  height: MediaQuery.of(context).size.height * 0.07,
                   decoration: BoxDecoration(
                     color: Color(0xFFEEEEEE),
                   ),
@@ -36,136 +35,86 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
                     padding: EdgeInsets.zero,
                     scrollDirection: Axis.vertical,
                     children: [
-                      Card(
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-                        color: Color(0xFFF5F5F5),
-                        child: Padding(
-                          padding: EdgeInsets.fromLTRB(5, 3, 3, 3),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Row(
-                                mainAxisSize: MainAxisSize.max,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 1),
-                                    child: Text(
-                                      '出発 :',
-                                      style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                    child: Text(
-                                      '九大学研都市駅',
-                                      style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                        fontSize: 15,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              Row(
-                                mainAxisSize: MainAxisSize.max,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 1),
-                                    child: Text(
-                                      '到着 :',
-                                      style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                    child: Text(
-                                      '産学連携交流センター',
-                                      style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                        fontSize: 15,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                      Card(
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-                        color: Color(0xFFF5F5F5),
-                        child: Padding(
-                          padding: EdgeInsets.fromLTRB(5, 3, 3, 3),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Row(
-                                mainAxisSize: MainAxisSize.max,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 1),
-                                    child: Text(
-                                      '出発 :',
-                                      style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                    child: Text(
-                                      '九大学研都市駅',
-                                      style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                        fontSize: 15,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                              Row(
-                                mainAxisSize: MainAxisSize.max,
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                children: [
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(0, 0, 0, 1),
-                                    child: Text(
-                                      '到着 :',
-                                      style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                                    child: Text(
-                                      '産学連携交流センター',
-                                      style: TextStyle(
-                                        fontFamily: 'Poppins',
-                                        fontSize: 15,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                        ),
-                      )
+                      BusPairCard(),
+                      BusPairCard(),
+                      BusPairCard(),
+                      BusPairCard()
                     ],
                   ),
                 ),
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class BusPairCard extends StatelessWidget {
+  BusPair bus_pair = BusPair(origin: "九大学研都市駅", destination: "産学連携交流センター");
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      color: Color(0xFFF5F5F5),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(5, 3, 3, 3),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 1),
+                  child: Text(
+                    '出発 :',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Text(
+                    bus_pair.origin,
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 15,
+                    ),
+                  ),
+                )
+              ],
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 1),
+                  child: Text(
+                    '到着 :',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                  child: Text(
+                    bus_pair.destination,
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 15,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
