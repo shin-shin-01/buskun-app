3b46ce1589848a8fb1318c55fa537f5a4e4161d9
Kaze Shindo <kaze.bknb@outlook.jp>
2021-08-11 11:59:23 +0900

    add doc.id to busPair

--- lib/model/bus_pair.dart
+++ lib/model/bus_pair.dart
@@ -1,8 +1,10 @@
 class BusPair {
+  final String id;
   final String origin;
   final String destination;
 
   BusPair({
+    required this.id,
     required this.origin,
     required this.destination,
   });
