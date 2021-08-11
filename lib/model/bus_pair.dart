5877beae88119371db1f0c52f083f3344f448dda
Kaze Shindo <kaze.bknb@outlook.jp>
2021-08-11 12:00:25 +0900

    create BusPair from firestoreDoc

--- lib/model/bus_pair.dart
+++ lib/model/bus_pair.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class BusPair {
   final String id;
   final String origin;
@@ -8,4 +10,13 @@ class BusPair {
     required this.origin,
     required this.destination,
   });
+
+  factory BusPair.fromFirestore(QueryDocumentSnapshot firestoreDoc) {
+    final data = firestoreDoc.data();
+    return BusPair(
+      id: firestoreDoc.id,
+      origin: data['origin'] as String,
+      destination: data['destination'] as String,
+    );
+  }
 }
