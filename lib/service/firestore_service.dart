d910fd8c3d1657d02c888c427ef8343438837c92
Kaze Shindo <kaze.bknb@outlook.jp>
2021-08-11 12:04:38 +0900

    imple func to link FavoriteBus and Firestore

--- lib/service/firestore_service.dart
+++ lib/service/firestore_service.dart
@@ -1,8 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../model/timetable.dart';
+import '../model/bus_pair.dart';
 
 class FirestoreService {
   CollectionReference buses = FirebaseFirestore.instance.collection('buses');
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
 
   // ["九大学研都市駅", "産学連携交流センター", ... ]
   Future<List<String>> getOriginBusstopIds() async {
@@ -27,5 +29,25 @@ class FirestoreService {
         .then((QuerySnapshot querySnapshot) => querySnapshot.docs
             .map((doc) => Timetable.fromFirestore(doc))
             .toList());
+  }
+
+  // ユーザーのお気に入りのバス停組み合わせ一覧を取得
+  Future<List<BusPair>> getFavoriteBusPairs(String userId) async {
+    return await users.doc(userId).collection("favorites").get().then(
+        (QuerySnapshot querySnapshot) => querySnapshot.docs
+            .map((doc) => BusPair.fromFirestore(doc))
+            .toList());
+  }
+
+  // お気に入りバス停を追加
+  void setFavoriteBusPair(String userId, String origin, String destination) {
+    users.doc(userId).collection("favorites").doc().set(
+        {"origin": origin, "destination": destination},
+        SetOptions(merge: true));
+  }
+
+  // お気に入りバス停を削除
+  Future<void> deleteFavoriteBusPair(String userId, BusPair busPair) async {
+    await users.doc(userId).collection("favorites").doc(busPair.id).delete();
   }
 }
