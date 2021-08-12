f2a79a5754dd1fd6e30fddf5f615422f9a152f81
Kaze Shindo <kaze.bknb@outlook.jp>
2021-08-12 12:11:48 +0900

    add isHoliday to TimeTable

--- lib/model/timetable.dart
+++ lib/model/timetable.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Timetable {
+  final bool isHoliday;
   final String line;
 
   final String departureAt;
@@ -8,11 +9,15 @@ class Timetable {
   final String arriveAt;
 
   Timetable(
-      {required this.line, required this.departureAt, required this.arriveAt});
+      {required this.isHoliday,
+      required this.line,
+      required this.departureAt,
+      required this.arriveAt});
 
   factory Timetable.fromFirestore(QueryDocumentSnapshot firestoreDoc) {
     final data = firestoreDoc.data();
     return Timetable(
+      isHoliday: data["holiday"] as bool,
       line: data['line'] as String,
       departureAt: data['departureAt'] as String,
       arriveAt: data['arriveAt'] as String,
